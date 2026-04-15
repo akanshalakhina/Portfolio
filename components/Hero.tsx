ad8edcf9aca82cc6b752a512945b2129be95fd2a
Akansha Lakhina <akanshalakhina23@gmail.com>
2026-04-15 15:47:23 +0530

    Updated Hero Section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Spotlight } from "./ui/Spotlight";
 import { cn } from "@/utils/cn";
-import { TextGenerateEffectDemo } from "./ui/TextGenerateEffect";
+import { TextGenerateEffect } from "./ui/TextGenerateEffect";
 
 function Hero() {
   return (
@@ -27,9 +27,14 @@ function Hero() {
             <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
                 Dynamic Web Magic with Next.js
             </h2>
-            <TextGenerateEffectDemo 
+            <TextGenerateEffect 
             className="text-center text-[40px] md:text-5xl lg:text-6xl"
-            words="Transforming concepts into seamless experiences"/>
+            words="Transforming concepts into Seamless experiences"
+            />
+            <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
+              Hi, I'm Akansha , a passionate web developer based at Gurgaon.
+
+            </p>
 
         </div>
 
